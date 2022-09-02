e9fcb506b283a4b671ad496f0d265acbd7f9c7f4
l3on3l <fabianvera045@gmail.com>
2022-09-02 16:47:31 -0400

    added windows functions for draw the lines

--- wu_aaline.py
+++ wu_aaline.py
@@ -1,6 +1,7 @@
 import pygame, sys
 from pygame.locals import *
 from sys import argv
+import time
 
 #set up windows
 WINDOW_HEIGHT = 800
@@ -30,7 +31,7 @@ def leer_puntos(archivo='./dino.txt'):
         datos.close()
     return puntos
 
-def poligono(canvas, lista_puntos):
+def poligono(lista_puntos):
     puntos = lista_puntos
     p1 = puntos[0]
     linea = 0
@@ -39,26 +40,26 @@ def poligono(canvas, lista_puntos):
         draw_line(p1, p2)
         p1 = p2
 
-def putPixel(point, color):
+def put_pixel(point, color):
     """dibuja un pixel en la ventana"""
     global pixel_size
     x, y = point
     rect = pygame.Rect(x*pixel_size, y*pixel_size, pixel_size, pixel_size)
     pygame.draw.rect(DISPLAYSURF, color, rect)
 
-def resetWindow():
+def reset_window():
     """re-dibuja todos los pixeles de la ventana"""
     global grid
     for y in range(WINDOW_HEIGHT):
         for x in range(WINDOW_WIDTH):
-            putPixel((x,y), BG_COLOR)
+            put_pixel((x,y), BG_COLOR)
 
 def fill_colour(point, alpha=1):
     """pinta el pixel en (x, y) con transparencia alpha (donde 0≤ alpha ≤ 1)"""
     global color
     def rgba_color(bg, fg): return round(alpha * fg + (1-alpha) * bg)
     colour = tuple(map(rgba_color, DISPLAYSURF.get_at(point)[:3], color))
-    putPixel(point, colour)
+    put_pixel(point, colour)
 
 def fpart(x):
     """retorna parte fraccional de x"""
@@ -88,7 +89,7 @@ def draw_endpoint(point, gradient, steep):
     fill_colour(calc_point(xpxl, ypxl+1, steep), fpart(yend) * xgap)
     return xpxl
 
-def draw_line(point1:tuple, point2:tuple):
+def draw_line(point1:tuple, point2:tuple, animated:bool=False):
     """Dibuja una linea anti-aliased desde point1 a point2 con el algoritmo xiaolin wu's."""
     x1, y1 = point1
     x2, y2 = point2
@@ -129,13 +130,19 @@ def draw_line(point1:tuple, point2:tuple):
         fill_colour(calc_point(x, y, steep), rfpart(intery))
         fill_colour(calc_point(x, y+1, steep), fpart(intery))
         intery += gradient
+        # solo para animacion con pygame
+        if animated:
+            pygame.display.update()
+            time.sleep(1)
 
 def window_simple_line(point1, point2):
     "dibuja una linea simple desde el point1 a point2 con pygame"
     global pixel_size
+
     print("\n\tZoom config:")
     print("\t\tzoom (+): space")
     print("\t\tzoom (-): backspace")
+
     while True:        
             for event in pygame.event.get():
                 if event.type == QUIT:
@@ -148,13 +155,42 @@ def window_simple_line(point1, point2):
                     if event.key == K_BACKSPACE:
                         if (pixel_size != MIN_PIXEL_SIZE):
                             pixel_size -= 1
-                    resetWindow()
-            # poligono(DISPLAYSURF,leer_puntos(archivo='./dino.txt'))
+                    reset_window()
             draw_line(point1, point2)
             pygame.display.update()
 
+def window_draw_polygon(file):
+    "dibuja un poligono con el algoritmo de wu's de acuerdo a la serie de puntos del file"
+    while True:
+        for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    sys.exit()
+        poligono(leer_puntos(archivo=file))
+        pygame.display.update()
+
+def window_animated_line():
+    "dibuja una linea con animacion"
+    global pixel_size
+    pixel_size = 15
+    
+    while True:
+        for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    sys.exit()
+        pygame.draw.aaline(DISPLAYSURF, WHITE, (5*pixel_size, 10*pixel_size), (20*pixel_size,20*pixel_size))
+        draw_line((5, 10), (20, 20), True)
+        reset_window()
+        
 if __name__ == '__main__':
     global FPSCLOCK, grid, pixel_size
+
+    if (len(argv) < 2 or len(argv) > 4):
+        print(f"Usage: {argv[0]} [-p | --point] <x1-y1> <x2-y2>  dibuja una linea desde el punto <x1-y1> al <x2-y2>")
+        print(f"Usage: {argv[0]} [-f | --file] <path>            dibuja un poligono con los puntos del archivo <path>")
+        print(f"Usage: {argv[0]} [-a | --animated]               dibuja una linea animada para la demostracion del algortimo")
+        sys.exit()
 
     pygame.init()
 
@@ -165,11 +201,25 @@ if __name__ == '__main__':
     color = RED
     pixel_size = 1
     
-    # draw_line2((90, 100), (120, 120)) #para el ej
-    # game loop
-    print(argv)
     if '-p' in argv or '--point' in argv:
         if len(argv) == 4:
             point1 = tuple(map(int, argv[2].split('-')))
             point2 = tuple(map(int, argv[3].split('-')))
             window_simple_line(point1, (point2))
+        else:
+            print(f"Usage: {argv[0]} [-p | --point] <x1-y1> <x2-y2>")
+    elif '-f' in argv or '--file' in argv:
+        if len(argv) == 3:
+           window_draw_polygon(argv[2])
+        else:
+            print(f"Usage: {argv[0]} [-f | --file] <path>")
+    elif '-a' in argv or '--animated' in argv:
+        if len(argv) == 2:
+            window_animated_line()
+        else:
+            print(f"Usage: {argv[0]} [-a | --animated]")
+    else:
+        print(f"Usage: {argv[0]} [-p | --point] <x1-y1> <x2-y2>  dibuja una linea desde el punto <x1-y1> al <x2-y2>")
+        print(f"Usage: {argv[0]} [-f | --file] <path>            dibuja un poligono con los puntos del archivo <path>")
+        print(f"Usage: {argv[0]} [-a | --animated]               dibuja una linea animada para la demostracion del algortimo")
+
